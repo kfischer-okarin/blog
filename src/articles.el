4ae277a240ee3959786606a7eab720663f1e085b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-01-14 23:15:19 +0900

    Also add hash of mermaid code content to the filename

--- src/articles.el
+++ src/articles.el
@@ -7,3 +7,15 @@
        (replace-regexp-in-string
         "[^[:alnum:] ]" ""
         (downcase headline))))))
+
+(defun kf-org-babel-src-block-body-hash-abbrev ()
+  (let* ((src-block-body (org-element-property :value (org-element-at-point)))
+         (hash (sha1 src-block-body)))
+    (substring hash 0 8)))
+
+(defun kf-mermaid-result-filename ()
+  (concat "images/generated/"
+          (kf-org-current-headline-in-snakecase)
+          "-"
+          (kf-org-babel-src-block-body-hash-abbrev)
+          ".svg"))
