6952ef55844ccbe4c81d0b859a76ea5c12681cb8
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-02-17 13:09:26 +0900

    Rewrite tests with more straightforward assertions

--- src/export-backend-test.el
+++ src/export-backend-test.el
@@ -1,36 +1,78 @@
+(defun concat-lines (&rest lines)
+  (mapconcat
+   (lambda (line) (if (string-empty-p line) "" line))
+   lines
+   "\n"))
+
+(defun export-org-lines (&rest lines)
+  (let ((org-export-website-template-string "{{ content }}")
+        (org-content (apply #'concat-lines lines)))
+    (with-temp-buffer
+      (insert org-content)
+      (string-trim-right (org-export-as org-export-website-backend)))))
+
+(defun article-headline (title &optional published-at)
+  (concat-lines (concat "* " title)
+                ":PROPERTIES:"
+                (concat ":PUBLISHED_AT: " (or published-at "[2024-02-16 Fri 00:21]"))
+                ":END:"))
+
 (ert-deftest test-org-export-website-headline ()
-  (should (equal (org-export-website-headline
-                  '(headline (:raw-value "foo" :level 1 :PUBLISHED_AT "[2024-02-16 Fri 00:21]")) nil nil)
+  (should (equal (export-org-lines "* foo"
+                                   ":PROPERTIES:"
+                                   ":PUBLISHED_AT: [2024-02-16 Fri 00:21]"
+                                   ":END:")
                  "<h1>foo</h1>"))
-  (should (equal (org-export-website-headline
-                  '(headline (:raw-value "foo" :level 1)) nil nil)
-                 nil))
-  (should (equal (org-export-website-headline
-                  '(headline (:raw-value "bar" :level 2)) nil nil)
-                 "<h2>bar</h2>"))
-  (should (equal (org-export-website-headline
-                  '(headline (:raw-value "baz" :level 3)) "Some content" nil)
-                 "<h3>baz</h3>\n\nSome content")))
-
-(ert-deftest test-org-export-website-headline-level-1-resets-footnotes ()
-  (let ((info '(:footnotes ("a" "b"))))
-    (org-export-website-headline
-     '(headline (:raw-value "foo" :level 1 :PUBLISHED_AT "[2024-02-16 Fri 00:21]")) nil info)
-    (should (equal (plist-get info :footnotes) nil))))
-
-(ert-deftest test-org-export-website-headline-level-2-does-not-reset-footnotes ()
-  (let ((info '(:footnotes ("a" "b"))))
-    (org-export-website-headline
-     '(headline (:raw-value "foo" :level 2 :PUBLISHED_AT "[2024-02-16 Fri 00:21]")) nil info)
-    (should (equal (plist-get info :footnotes) '("a" "b")))))
+  (should (equal (export-org-lines "* foo") ""))
+  (should (equal (export-org-lines "** bar") "<h2>bar</h2>"))
+  (should (equal (export-org-lines "*** baz"
+                                   "Some content")
+                 (concat-lines "<h3>baz</h3>"
+                               ""
+                               "<p>Some content</p>"))))
 
 (ert-deftest test-org-export-website-headline-footnotes-are-attached-after-level-1-headlines ()
-  (should (equal
-           (org-export-website-headline
-            '(headline (:raw-value "foo" :level 1 :PUBLISHED_AT "[2024-02-16 Fri 00:21]"))
-            nil
-            '(:footnotes ("a" "b")))
-            "<h1>foo</h1>\n\n<div class=\"footnotes\"><ol><li>a</li><li>b</li></ol></div>")))
+  (should (equal (export-org-lines (article-headline "foo")
+                                   "** Subheader"
+                                   "Something secret[fn::not telling you]")
+                 (concat-lines "<h1>foo</h1>"
+                               ""
+                               "<h2>Subheader</h2>"
+                               ""
+                               "<p>Something secret<sup><a href=\"#fn-1\" id=\"fnref-1\">[1]</a></sup></p>"
+                               ""
+                               ""
+                               "<div class=\"footnotes\"><ol><li><a href=\"#fnref-1\" id=\"fn-1\">[1]</a> not telling you</li></ol></div>"))))
+
+(ert-deftest test-org-export-website-link-http-https ()
+  (should (equal (export-org-lines "[[http://example.com][Link Text]]")
+                 "<p><a href=\"http://example.com\">Link Text</a></p>"))
+  (should (equal (export-org-lines "[[https://example.com][Link Text 2]]")
+                 "<p><a href=\"https://example.com\">Link Text 2</a></p>"))
+  (should (equal (export-org-lines "[[http://example.com]]")
+                 "<p><a href=\"http://example.com\">http://example.com</a></p>")))
+
+(ert-deftest test-org-export-website-link-image-file ()
+  (should (equal (export-org-lines "[[file:./image.png][Image Text]]")
+                 "<p><img src=\"./image.png\" alt=\"Image Text\" /></p>"))
+  (should (equal (export-org-lines "[[file:./image.png]]")
+                 "<p><img src=\"./image.png\" /></p>")))
+
+(ert-deftest test-org-export-website-link-video-file ()
+  (should (equal (export-org-lines "[[file:./video.mp4][Video Text]]")
+                                   "<p><figure><video controls width=\"640\"><source src=\"./video.mp4\" type=\"video/mp4\" /></video><figcaption>Video Text</figcaption></figure></p>")))
+
+(ert-deftest test-org-export-website-italic ()
+  (should (equal (export-org-lines "/foo/")
+                 "<p><em>foo</em></p>")))
+
+(ert-deftest test-org-export-website-src-block ()
+  (should (equal (export-org-lines "#+begin_src python"
+                                   "  print('foo')"
+                                   ""
+                                   "  1 + 2"
+                                   "#+end_src")
+                 "<pre><code class=\"language-python\">print('foo')\n\n1 + 2\</code></pre>")))
 
 (ert-deftest test-regexp-match-column ()
   (should (equal (regexp-match-column "b\\w\\w" "foo\nfoo bar baz") 4)))
@@ -44,39 +86,3 @@
 (ert-deftest test-replace-placeholder-with-indent ()
   (should (equal (replace-placeholder-with-indent "{{here}}" "content1\ncontent2" "foo\n  {{here}}\nbaz")
                  "foo\n  content1\n  content2\nbaz")))
-
-(ert-deftest test-org-export-website-link-http-https ()
-  (should (equal
-           (org-export-website-link '(link (:raw-link "http://example.com" :type "http")) "Link Text" nil)
-           "<a href=\"http://example.com\">Link Text</a>"))
-  (should (equal
-           (org-export-website-link '(link (:raw-link "https://example.com" :type "https")) "Link Text 2" nil)
-           "<a href=\"https://example.com\">Link Text 2</a>"))
-  (should (equal
-           (org-export-website-link '(link (:raw-link "http://example.com") :type "http") nil nil)
-           "<a href=\"http://example.com\">http://example.com</a>")))
-
-(ert-deftest test-org-export-website-link-image-file ()
-  (should (equal
-           (org-export-website-link
-            '(link (:raw-link "file:./image.png" :path "./image.png" :type "file")) "Image Text" nil)
-           "<img src=\"./image.png\" alt=\"Image Text\" />"))
-  (should (equal
-           (org-export-website-link
-            '(link (:raw-link "file:./image.png" :path "./image.png" :type "file")) nil nil)
-           "<img src=\"./image.png\" />")))
-
-(ert-deftest test-org-export-website-link-video-file ()
-  (should (equal
-           (org-export-website-link
-            '(link (:raw-link "file:./video.mp4" :path "./video.mp4" :type "file")) "Video Text" nil)
-           "<figure><video controls width=\"640\"><source src=\"./video.mp4\" type=\"video/mp4\" /></video><figcaption>Video Text</figcaption></figure>")))
-
-(ert-deftest test-org-export-website-italic ()
-  (should (equal (org-export-website-italic '(italic) "foo" nil)
-                 "<em>foo</em>")))
-
-(ert-deftest test-org-export-website-src-block ()
-  (should (equal (org-export-website-src-block
-                  '(src-block (:language "python" :value "  print('foo')\n\n  1 + 2")) nil nil)
-                  "<pre><code class=\"language-python\">print('foo')\n\n1 + 2</code></pre>")))
